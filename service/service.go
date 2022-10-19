bfdcffdee469fa79ffe9ac154d0645203744dc5d
Dokkabei97 <wkdrn970@naver.com>
2022-10-19 09:55:33 +0900

    feat: 테스트 코드 ioutil deprecated 코드로 os로 변경 #1

--- service/service.go
+++ service/service.go
@@ -2,44 +2,57 @@ package service
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"is-deploy-agent/model"
 	"log"
 	"os"
-	"is-deploy-agent/model"
 )
+
 func readFile(path string) {
-	file, err := ioutil.ReadFile(path)
+	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(file))
+
+	file.Close()
 }
 
 func writeFile(path string, lb string) {
-	ioutil.WriteFile(path, []byte(lb), os.FileMode(644))
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.Write([]byte(lb))
+	file.Close()
 }
 
 func readJson() string {
-	path, err := ioutil.ReadFile("./setting.json")
+	path, err := os.Open("./setting.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var model []model.Model
-	json.Unmarshal(path, &model)
+
+	decoder := json.NewDecoder(path)
+	decoder.Decode(&model)
 
 	key := model[0].PodList[0].LbMap[0].Key
 	value := model[0].PodList[0].LbMap[0].Value
 
 	result := key + "=" + value
+
 	return result
 }
 
-func Restore()  {
+func Restore() {
 	path := "../ex.properties"
 	lb := readJson()
 	readFile(path)
 	writeFile(path, lb)
 	readFile(path)
+}
+
+func Exclude() {
+
 }
