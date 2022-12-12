766160aa0ecae93b34e3cc236f890a9b65829c17
Dokkabei97 <wkdrn970@naver.com>
2022-12-13 08:30:11 +0900

    feat: update github actions & gitlab-ci

--- update2.sh
+++ update2.sh
@@ -5,18 +5,14 @@ port=$1
 version=$2
 backup_dir=backup-dir
 
-# download agent
-wget https://github.com/danawalab/$agent/releases/download/$version/$agent-$version.tar.gz
 # kill old agent
 kill -9 $(ps -ef | grep $agent | awk '{print $2}')
 # delete old agent
 rm $agent
+# download agent
+wget https://github.com/danawalab/$agent/releases/download/$version/$agent-$version.tar.gz
 # unzip new agent.tar.gz
 tar -zxvf $agent-$version.tar.gz
-# create backup directory
-if [ ! -e $backup_dir ]; then
-  mkdir $backup_dir
-fi
 # move new agent.tar.gz  backup-dir directory
 mv $agent-$version.tar.gz $backup_dir
 # run new agent
